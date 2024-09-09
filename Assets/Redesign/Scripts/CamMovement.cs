816f190da15aabb4eedf9b3974ed608c785abe60
Luke Chelius <ldc1618@rit.edu>
2024-09-09 17:45:41 -0400

    Loaded project on new PC

--- Assets/Redesign/Scripts/CamMovement.cs
+++ Assets/Redesign/Scripts/CamMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
+
+    public Transform subject;  // What the cam is following
+    public float camFollowSpeed;  // How fast ("smooth") the camera follows
+    public Vector2 maxPos;  // Max camera position so it doesn't go out of bounds
+    public Vector2 minPos;  // Min camera position so it doesn't go out of bounds
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // LateUpdate is called once per frame last
+    void LateUpdate() {
+        // Compare cam position to what it's following's position, move it if different
+        if (transform.position != subject.position) {
+            // Create a position vector so that the z position of the cam is unchanged
+            Vector3 movePosition = new Vector3(subject.position.x,
+                                               subject.position.y, 
+                                               transform.position.z);
+
+            // Ensure the cam doesn't move out of the min or max boundaries
+            movePosition.x = Mathf.Clamp(movePosition.x, minPos.x, maxPos.x);
+            movePosition.y = Mathf.Clamp(movePosition.y, minPos.y, maxPos.y);
+
+            // Move the cam to the subject's position
+            transform.position = Vector3.Lerp(transform.position, movePosition, camFollowSpeed);
+        }
     }
 }
